0fd0dab60ba8cfbba1deccb12760b233f4041795
JHorlamide <olamidejubril68@gmail.com>
2022-09-27 22:39:06 +0100

    Refactored gate in and gateout cards

--- src/components/Inventory/GateInDataCard.js
+++ src/components/Inventory/GateInDataCard.js
@@ -12,111 +12,58 @@ import {
 } from "./GateInStyles";
 import { AiOutlineMore } from "react-icons/ai";
 
-// const GateInCard = ({ inData, index }) => {
-//   return (
+// const GateInCard = ({ gateInData }) => {
+//   return gateInData.map((gateItem, index) => (
 //     <Card key={index}>
 //       <CardTop>
-//         <CardId>{inData.containerNumber}</CardId>
-//         <CardStatus>{inData.containerCondition}</CardStatus>
+//         <CardId>{gateItem.containerNumber}</CardId>
+//         <CardStatus>{gateItem.containerCondition}</CardStatus>
 //       </CardTop>
 
-//       <CardMiddle>{inData.containerLocation}</CardMiddle>
+//       <CardMiddle>{gateItem.containerLocation}</CardMiddle>
 
 //       <CardBottom>
 //         <CardDates>
-//           <p>{inData.gateInDate}</p>
-//           <p>{inData.dateOut}</p>
+//           <p>{gateItem.gateInDate}</p>
+//           <p>{gateItem.dateOut}</p>
 //         </CardDates>
 //         <CardIcon>
 //           <AiOutlineMore />
 //         </CardIcon>
 //         <div>
-//           <p>{inData.truckNumber}</p>
-//           <p>{inData.containerStatus}</p>
+//           <p>{gateItem.truckNumber}</p>
+//           <p>{gateItem.containerStatus}</p>
 //         </div>
 //       </CardBottom>
 //     </Card>
-//   );
+//   ));
 // };
 
-const GateInCard = ({ gateInData }) => {
-  return gateInData.map((gateItem, index) => (
-    <Card key={index}>
-      <CardTop>
-        <CardId>{gateItem.containerNumber}</CardId>
-        <CardStatus>{gateItem.containerCondition}</CardStatus>
-      </CardTop>
-
-      <CardMiddle>{gateItem.containerLocation}</CardMiddle>
-
-      <CardBottom>
-        <CardDates>
-          <p>{gateItem.gateInDate}</p>
-          <p>{gateItem.dateOut}</p>
-        </CardDates>
-        <CardIcon>
-          <AiOutlineMore />
-        </CardIcon>
-        <div>
-          <p>{gateItem.truckNumber}</p>
-          <p>{gateItem.containerStatus}</p>
-        </div>
-      </CardBottom>
-    </Card>
-  ));
-};
-
-// const GateOutCard = ({ outData }) => {
-//   return (
-//     <Card>
+// const GateOutCard = ({ gateOutData }) => {
+//   return gateOutData.map((gateOutItems, index) => (
+//     <Card key={index}>
 //       <CardTop>
-//         <CardId>{outData.containerNumber}</CardId>
-//         <CardStatus>{outData.containerCondition}</CardStatus>
+//         <CardId>{gateOutItems.containerNumber}</CardId>
+//         <CardStatus>{gateOutItems.containerCondition}</CardStatus>
 //       </CardTop>
 
-//       <CardMiddle>{outData.containerStatus}</CardMiddle>
+//       <CardMiddle>{gateOutItems.containerStatus}</CardMiddle>
 
 //       <CardBottom>
 //         <CardDates>
-//           <p>{outData.dateOut}</p>
+//           <p>{gateOutItems.dateOut}</p>
 //         </CardDates>
 //         <CardIcon>
 //           <AiOutlineMore />
 //         </CardIcon>
 //         <div>
-//           <p>{outData.truckNumber}</p>
-//           <p>{outData.containerStatus}</p>
+//           <p>{gateOutItems.truckNumber}</p>
+//           <p>{gateOutItems.containerStatus}</p>
 //         </div>
 //       </CardBottom>
 //     </Card>
-//   );
+//   ));
 // };
-
-const GateOutCard = ({ gateOutData }) => {
-  return gateOutData.map((gateOutItems, index) => (
-    <Card key={index}>
-      <CardTop>
-        <CardId>{gateOutItems.containerNumber}</CardId>
-        <CardStatus>{gateOutItems.containerCondition}</CardStatus>
-      </CardTop>
-
-      <CardMiddle>{gateOutItems.containerStatus}</CardMiddle>
-
-      <CardBottom>
-        <CardDates>
-          <p>{gateOutItems.dateOut}</p>
-        </CardDates>
-        <CardIcon>
-          <AiOutlineMore />
-        </CardIcon>
-        <div>
-          <p>{gateOutItems.truckNumber}</p>
-          <p>{gateOutItems.containerStatus}</p>
-        </div>
-      </CardBottom>
-    </Card>
-  ));
-};
 
 const DataCard = ({ cardData, label}) => {
   return cardData.map((gateItem, index) => (
--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,13 +1,9 @@
 import GateInDataCard from "./GateInDataCard";
-// import GateOutDataCard from "./GateOutDataCard";
-// import InventorySearch from "./InventorySearch";
 
 function Inventory({ gateInData, gateOutData }) {
   return (
     <div>
-      {/* <InventorySearch /> */}
       <GateInDataCard gateInData={gateInData} gateOutData={gateOutData} />
-      {/* <GateOutDataCard gateOutData={gateOutData} /> */}
     </div>
   );
 }
